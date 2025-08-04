c224ddcfaf91ca0a7d57ae9dd0552c563f3c53f6
OATHTAKER <840814316@qq.com>
2025-08-04 23:23:17 +0100

    add wrapper

---
+++ src/wrapper.cpp
@@ -0,0 +1,33 @@
+#include <pybind11/pybind11.h>
+#include <pybind11/stl.h> 
+#include "Microfacet.hpp"
+#include "Math.hpp"            
+
+namespace py = pybind11;
+
+
+PYBIND11_MODULE(ndf_py, m) {
+    m.doc() = "Python bindings for generating dataset of machine learning";
+    
+    //Vec2f
+    py::class_<Vec2f>(m, "Vec2f")
+        .def(py::init<float, float>(), "Constructor for a 2D vector")
+        .def_readwrite("x", &Vec2f::x)
+        .def_readwrite("y", &Vec2f::y);
+
+    //Vec3f
+    py::class_<Vec3f>(m, "Vec3f")
+        .def(py::init<float, float, float>(), "Constructor for a 3D vector")
+        .def_readwrite("x", &Vec3f::x)
+        .def_readwrite("y", &Vec3f::y)
+        .def_readwrite("z", &Vec3f::z);
+
+    //NDF
+    py::class_<PiecewiseLinearNDF>(m, "PiecewiseLinearNDF")
+        //constructor needs Dx and Dy
+        .def(py::init<const std::vector<float>&, const std::vector<float>&>(),
+            "Constructor taking two lists of ControlPoints for Dx and Dy")
+        //G1
+        .def("G1", &PiecewiseLinearNDF::G1, "Calculates the Smith G1 shadowing-masking function",
+            py::arg("w"), py::arg("wh"));
+}
